710b23548bab8ace09204ec897d02d2fa4905c5a
Sack <rauldaxo@gmail.com>
2024-06-05 17:35:32 -0600

    Specific SpawnRandomAnimal time function
    Also, we do not need to use S key in order to create animals. We have a new method called Invoke Repeating

--- Assets/Course Library/Scripts/SpawnManager.cs
+++ Assets/Course Library/Scripts/SpawnManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -16,12 +18,16 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 20;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPosition,
-            animalPrefabs[animalIndex].transform.rotation);
-        }
+        // if (Input.GetKeyDown(KeyCode.S)) // NO IN USE ANYMORE
+        // {
+        //     SpawnRandomAnimal();
+        // }
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPosition,
+        animalPrefabs[animalIndex].transform.rotation);
     }
 }
