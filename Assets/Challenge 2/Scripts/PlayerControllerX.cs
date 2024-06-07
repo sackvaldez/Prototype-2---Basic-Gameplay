34f608a07451a63e5771828834a15467a5512fcb
Sack <rauldaxo@gmail.com>
2024-06-06 18:48:40 -0600

    Challenge 2: Play Fetch
    In this section, we took some prefabricated scene of a player that has to send dogs in order to catch the balls. Here is what we did to practice our learned skills:
    
    1. Dogs were generated at the top of the screen because it was not the correct prefab. Fixed.
    
    2.  Exchanged Dog and Ball because player was sending the wrong prefab.
    
    3. Dog Collider adjusted because it was too big and it can easily catch the balls.
    
    4. Updated DestroyOutOfBounds script with correct measures in order to eliminate Dog and Ball clones.
    
    5. Added a Random.Range(0, index.length) to Ball spawner because it was only generating one type of ball. Similar with generationInterval in SpawnManager script.
    
    6. The most challenging part was to set a timer. Got completed by adding an acumulative variable of time, using it in the condition of 1 second passed and updating it after instancing the dog. All inside the Update method.

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timeSinceLastDog = 0f; // Variable para acumular el tiempo
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastDog += Time.deltaTime; // Acumula el tiempo transcurrido
+
+        // On spacebar press, send dog if 1 second have passed
+        if (Input.GetKeyDown(KeyCode.Space) && timeSinceLastDog >= 1f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeSinceLastDog = 0f; // Resetea el tiempo acumulado después de instanciar el perro
+        }
+    }
+}
